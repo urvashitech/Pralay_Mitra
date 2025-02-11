752ff1ef6e864da0d94e98ff6409b6c2df53254f
urvashi <urvashikshatriya9@gmail.com>
2025-02-12 00:09:10 +0530

    render.yaml file  Added

--- pralayMitra/settings.py
+++ pralayMitra/settings.py
@@ -119,12 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
-# Ensure Django finds static files at the project level
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "prediction/static"),
 ]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
